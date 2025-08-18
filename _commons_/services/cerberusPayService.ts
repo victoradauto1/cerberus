b0f866d67360aab4e210370cb3e9232baaebd55f
victoradauto1 <victoradauto@yahoo.com.br>
2025-08-18 13:24:08 -0300

    - adição de getprovider, getContract, getSigner, getCustomers,  em CerberusPayService.ts (commons);

--- _commons_/services/cerberusPayService.ts
+++ _commons_/services/cerberusPayService.ts
@@ -1,4 +1,32 @@
-import { ethers } from "ethers";
-import {abi as ABI} from "./CerberusPay.json";
+import { ethers, Provider, JsonRpcProvider, Contract, Signer, TransactionResponse } from "ethers";
+import { abi as ABI } from "./CerberusPay.json";
 import ConfigBase from "../configBase";
 
+function getProvider(): Provider {
+  return new JsonRpcProvider("https://rpc-amoy.polygon.technology/");
+}
+
+function getContract(): Contract {
+  const provider = getProvider();
+  return new ethers.Contract(ConfigBase.CERBERUS_PAY_CONTRACT, ABI, provider);
+}
+
+function getSigner(): Contract {
+  const provider = getProvider();
+  const signer = new ethers.Wallet(ConfigBase.ADMIN_PRIVATE_KEY, provider);
+  return new ethers.Contract(ConfigBase.CERBERUS_PAY_CONTRACT, ABI, signer);
+}
+
+export function getCustomers(): Promise<string[]> {
+  return getContract().getCustomers();
+}
+
+export function getCustomerNextPayment(customer: string): Promise<number> {
+  return getContract().payments(customer);
+}
+
+export async function pay(customer: string): Promise<string> {
+  const tx: TransactionResponse= await getSigner().pay(customer);
+  await tx.wait();
+  return tx.hash;
+}
