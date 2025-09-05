8b7a8a58e8be10cadc7107eaf4139ab081e63be9
victoradauto1 <victoradauto@yahoo.com.br>
2025-09-05 14:30:17 -0300

    - adição das funções updateAutomation e getAutomation de automation.service.ts em automation (backend);

--- backend/src/automation/automation.controller.ts
+++ backend/src/automation/automation.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthService } from '../auth/auth.service';
 import { PoolService } from '../pool/pool.service';
@@ -32,16 +41,62 @@ export class AutomationController {
       automation,
     );
 
-    if (automation.isActive) {
+    if (automation.isActive && automation.poolId) {
       const pool = await this.poolService.getPool(automation.poolId);
       const condition = automation.isOpened
         ? automation.closeCondition
         : automation.openCondition;
-      const tokenAddress = condition.field.indexOf('price0') !== -1 ? pool.token1 : pool.token0;
+      if (!condition) return response;
+      const tokenAddress =
+        condition.field.indexOf('price0') !== -1 ? pool.token1 : pool.token0;
 
       //pre aprovação do swap
     }
 
     return response;
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch(':id')
+  async updateAutomation(
+    @Param('id') id: string,
+    @Body() automation: AutomationDTO,
+    @Headers('Authorization') authorization: string,
+  ) {
+    const jwt = this.authService.decodeToken(authorization);
+    const user = await this.userService.getUserById(jwt.userId);
+    if (!user.privateKey)
+      throw new Error(
+        'You must have a private key is settings before you update a automation.',
+      );
+
+    const automationResult = await this.automationService.updateAutomation(
+      id,
+      jwt.userId,
+      automation,
+    );
+
+    if (!automationResult.poolId || !automationResult.isActive)
+      return automationResult;
+
+    const condition = automation.isOpened
+      ? automation.closeCondition
+      : automation.openCondition;
+
+    if (!condition) return automationResult;
+
+    const pool = await this.poolService.getPool(automation.poolId);
+    const tokenAddress =
+      condition.field.indexOf('price0') !== -1 ? pool.token1 : pool.token0;
+
+    //pegar a allownce
+    //pre aprovação do swap
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':id')
+  async getAutomation(@Param("id") id: string,@Headers('Authorization') authorization: string ){
+     const jwt = this.authService.decodeToken(authorization);
+     return this.automationService.getAutomation(id, jwt.userId);
   }
 }
--- backend/src/automation/automation.service.ts
+++ backend/src/automation/automation.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import Automation from 'commons/models/automation';
+import db from '../db';
 import { AutomationDTO } from './automation.dto';
-import Automation from "commons/models/automation";
-import db from "../db";
 
 @Injectable()
 export class AutomationService {
-  async addAutomation(userId: string, automation: AutomationDTO): Promise<Automation> {
+
+  async getAutomation(id: string, userId: string): Promise<Automation | null> {
+    return db.automations.findFirst({
+      where:{id, userId}
+    })
+  }
+
+  async addAutomation(
+    userId: string,
+    automation: AutomationDTO,
+  ): Promise<Automation> {
     return db.automations.create({
-      data:{
+      data: {
         exchange: automation.exchange,
         network: automation.network,
         openCondition: automation.openCondition,
@@ -17,8 +27,29 @@ export class AutomationService {
         name: automation.name,
         nextAmount: automation.nextAmount,
         poolId: automation.poolId,
-        userId
-      }
-    })
+        userId,
+      },
+    });
+  }
+
+  async updateAutomation(
+    id: string,
+    userId: string,
+    automation: AutomationDTO,
+  ): Promise<Automation> {
+    return db.automations.update({
+      where: { userId, id },
+      data: {
+        exchange: automation.exchange,
+        network: automation.network,
+        openCondition: automation.openCondition,
+        closeCondition: automation.closeCondition,
+        isActive: automation.isActive || false,
+        isOpened: automation.isOpened || false,
+        name: automation.name,
+        nextAmount: automation.nextAmount,
+        poolId: automation.poolId,
+      },
+    });
   }
 }
