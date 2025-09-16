cb6511355dd8d7d069c8d101ab90b5414d47ff41
victoradauto1 <victoradauto@yahoo.com.br>
2025-09-16 17:15:39 -0300

    - acrescimos de testes em automation.serice.spec.ts em test/automation (backend);

--- backend/test/automation/automation.service.spec.ts
+++ backend/test/automation/automation.service.spec.ts
@@ -1,9 +1,12 @@
-import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { automations } from 'commons/data';
 import { AutomationService } from 'src/automation/automation.service';
-
+import { prismaMock } from 'test/db.mock';
+import { newAutomationMock } from './automation.service.mock';
 
 describe('Automation Service tests', () => {
+  const userId = 'user123';
+
   let automationService: AutomationService;
 
   beforeAll(async () => {
@@ -12,6 +15,35 @@ describe('Automation Service tests', () => {
     }).compile();
 
     automationService = moduleFixture.get<AutomationService>(AutomationService);
+  });
+
+  it('should be defined', () => {
+    expect(automationService).toBeDefined();
+  });
+
+
+  it('should get automation', async () => {
+    prismaMock.automations.findUnique.mockResolvedValue({
+      ...newAutomationMock
+    } as automations);
+
+    const result = await automationService.getAutomation(
+      'automation123',
+      userId,
+    );
+    expect(result).toBeDefined();
+    expect(result!.id).toEqual(newAutomationMock.id);
+  });
+
+  it('should get automations', async () => {
+    prismaMock.automations.findMany.mockResolvedValue([{...newAutomationMock}] as automations[]);
+
+    const pageSize = 1;
+    const result = await automationService.getAutomations(userId,
+      1, pageSize
+    );
+    expect(result.length).toEqual(pageSize);
+    expect(result[0].id).toEqual(newAutomationMock.id);
   });
 
 });
