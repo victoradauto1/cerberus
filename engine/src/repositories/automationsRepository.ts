df1bce0313442a5556809e8a88ad882a6b90d98f
victoradauto1 <victoradauto@yahoo.com.br>
2025-09-24 13:45:42 -0300

    - adição da função getAutomation em automationRepository (engine);

--- engine/src/repositories/automationsRepository.ts
+++ engine/src/repositories/automationsRepository.ts
@@ -1,6 +1,12 @@
 import Automation from "commons/models/automation";
 import connect from "./db";
 
-async function getAutomation(id: string, userId: string): Promise<Automation | null>{
-
+async function getAutomation(
+  id: string,
+  userId: string
+): Promise<Automation | null> {
+  const db = await connect();
+  return db.automations.findUnique({
+    where: { id, userId },
+  });
 }
