ab3c7df94d28dfd5959712b621eaf87cc6eadc02
victoradauto1 <victoradauto@yahoo.com.br>
2025-09-25 12:34:43 -0300

    - finalização de automationRepository (engine); - ajuste em pay.ts em src (engine);

--- engine/src/pay.ts
+++ engine/src/pay.ts
@@ -3,6 +3,7 @@ import {getCustomerNextPayment, getCustomers, pay} from "commons/services/cerber
 import usersRepository from "./repositories/usersRepository";
 import { Status } from "commons/models/status";
 import sendMail from "./services/mailService"
+import automationsRepository from "./repositories/automationsRepository";
 
 async function executionCylce() {
     console.log("Executing the payment cycle...");
@@ -11,18 +12,26 @@ async function executionCylce() {
     console.log(customers.length + "customers loaded!");
 
     for (let i = 0; i < customers.length; i++){
-        const customerAddress = customers[0];
-        if(/0x0+/.test(customerAddress)) continue;
+        const customerAddress = customers[i].toLocaleLowerCase();
+        if(/^(0x0+)$/.test(customerAddress)) continue;
 
         const nextPayment = await getCustomerNextPayment(customerAddress);
         if(nextPayment > (Date.now()/1000)) continue;
 
+        let user;
+
         try {
           console.log("Chrging customer "+ customerAddress)
           await pay(customerAddress);
+          user = await usersRepository.updateUserStatus(customerAddress, Status.ACTIVE);
+          if(!user) continue;
+
+          await automationsRepository.startAutomations(user.id!);
         } catch (error) {
           const user = await usersRepository.updateUserStatus(customerAddress, Status.BLOCKED);
           if(!user) continue;
+
+          await automationsRepository.stopAutomations(user.id!);
 
           await sendMail(user.email, "Cerberus account blocked", `
             Hi, ${user.name}
@@ -35,11 +44,10 @@ async function executionCylce() {
 
             Admin
             `);
-
-            // to do: bloquear as automações
         }
     }
 
+    console.log("Finished the payment cycle.")
 }
 
 export default () => {
--- engine/src/repositories/automationsRepository.ts
+++ engine/src/repositories/automationsRepository.ts
@@ -1,5 +1,7 @@
 import Automation from "commons/models/automation";
 import connect from "./db";
+import { ChainId } from "commons/models/chainId";
+import { Exchange } from "commons/models/exchange";
 
 async function getAutomation(
   id: string,
@@ -9,4 +11,90 @@ async function getAutomation(
   return db.automations.findUnique({
     where: { id, userId },
   });
+}
+
+async function getAutomations(
+  userId: string,
+  network: ChainId,
+  exchange: Exchange
+): Promise<Automation[]> {
+  const db = await connect();
+  return db.automations.findMany({
+    where: { userId, network, exchange },
+  });
+}
+
+async function searchAutomations(poolId: string): Promise<Automation[]> {
+  const db = await connect();
+  return db.automations.findMany({
+    where: {
+      OR: [{ poolId }, { poolId: null }],
+      isActive: true,
+    },
+  });
+}
+
+async function addAutomation(automation: Automation): Promise<Automation> {
+  if (!automation.userId) throw new Error("Invalid automation userId.");
+  const db = await connect();
+  return db.automations.create({
+    data: new Automation(automation),
+  });
+}
+
+async function startAutomations(userId: string): Promise<void> {
+  const db = await connect();
+  await db.automations.updateMany({
+    where:{userId},
+    data: {isActive: true}
+  });
+}
+
+async function stopAutomations(userId: string): Promise<void> {
+  const db = await connect();
+  await db.automations.updateMany({
+    where:{userId},
+    data: {isActive: false}
+  });
+}
+
+async function updateAutomation(automationData: Automation): Promise<Automation | null>{
+  if (!automationData.userId || !automationData.id) throw new Error("Invalid automation userId and/or id.");
+  const db = await connect();
+  await db.automations.update({
+    where:{userId: automationData.userId, id: automationData.id},
+    data: {
+      exchange: automationData.exchange,
+      closeCondition: automationData.closeCondition,
+      openCondition: automationData.openCondition,
+      isActive: automationData.isActive,
+      isOpened: automationData.isOpened,
+      name: automationData.name,
+      network: automationData.network,
+      nextAmount: automationData.nextAmount,
+      poolId: automationData.poolId
+    }
+  });
+
+   return getAutomation(automationData.id, automationData.userId)
+}
+
+async function deleteAutomation(id: string, userId: string): Promise<boolean>{
+  const db = await connect();
+  await db.automations.delete({
+    where:{id, userId}
+  });
+
+  return true;
+}
+
+export default{
+  getAutomation,
+  getAutomations,
+  searchAutomations,
+  addAutomation,
+  startAutomations,
+  stopAutomations,
+  updateAutomation,
+  deleteAutomation
 }
