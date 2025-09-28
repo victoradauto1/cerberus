e6f18916a6649f218a25c582ddd0e8bbe483e4df
victoradauto1 <victoradauto@yahoo.com.br>
2025-09-28 19:17:28 -0300

    - adição de evalCondition e complementação de cerberus.ts em src (engine);

--- engine/src/cerberus.ts
+++ engine/src/cerberus.ts
@@ -1,7 +1,22 @@
 import Pool from "commons/models/pool";
+import automationsRepository from "./repositories/automationsRepository";
+import Automation from "commons/models/automation";
+
+function evalCondition(automation: Automation, pool: Pool): boolean{
+    return false;
+}
 
 export default async (pool:Pool): Promise<void>=>{
-    //bvuscar as automações
+
+    const automations = await  automationsRepository.searchAutomations(pool.id);
+    if(!automations || !automations.length) return;
+
+    console.log(`${automations.length}} automations found`);
+
+    automations.map(async (automation)=>{
+        const isValid = evalCondition(automation, pool);
+        if(!isValid) return;
+    })
 
     //testar as condições delas
 
